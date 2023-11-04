8c97c1e598f48e64b33339485af341185ccc382f
nicogkz <ngakrelidz@gmail.com>
2023-11-04 14:33:12 +0100

    Front init v2 (was not pushed)

---
+++ 2_ContainerizedApp/backend/api.py
@@ -0,0 +1,33 @@
+import pickle
+import pandas as pd
+from flask import Flask, request, jsonify
+
+
+data_path = "./data"
+model_path = "./model"
+model = pickle.load(open(model_path +'/automl.pkl', 'rb'))
+col_names = pickle.load(open(model_path +'/model_columns.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def testapi():
+    return jsonify({'status': 200,'msg': 'Welcome on this beautiful API!'})
+
+@app.route('/api/v1/estimate', methods=['POST'])
+def predict():
+    if request.is_json:
+        req_data = request.json
+        df = pd.DataFrame([req_data]).reindex(columns=col_names)
+        
+        prediction = list(model.predict(df.to_numpy()))
+
+        #print(prediction)
+
+        return jsonify({"prediction": str(prediction)},200)
+
+    else:
+        return jsonify({"error": "Content-Type is not 'application/json'"}, 400)
+
+if __name__ == 'main':
+    app.run(debug=True, port=5000)
--- Readme.md
+++ Readme.md
@@ -1,5 +1,19 @@
 ###
 
+Still WIP :)
+
+0/ 0_Investigation
+> Data exploration, data prep and AutoML Model using FLAML.
+> Status: Done.
+
+1/ 1_BackendAPI_test
+> Let's test to get an estimation using a Flask API End point. Runs on: http://127.0.0.1:5000
+> Status: WIP. Will add the retraining later.
+
+2/ 2_ContainerizedAPP
+> Let's add a basic front end to get estimations and containerized the whole app.
+> Status: WIP. Init.
+
 ## Data:
 > from UCI : https://archive.ics.uci.edu/dataset/477/real+estate+valuation+data+set
 
